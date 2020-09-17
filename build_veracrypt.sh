d0f3b050687cf48d33d5788d54c9f33e901f2085
srpedroborges <55651417+srpedroborges@users.noreply.github.com>
2020-09-17 10:13:03 +0100

    Add files via upload

---
+++ build_veracrypt.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+# Reference
+# https://github.com/veracrypt/VeraCrypt
+# 
+# https://github.com/wxWidgets/wxWidgets/blob/master/README-GIT.md
+# https://www.configserverfirewall.com/ubuntu-linux/mount-veracrypt-volume/
+
+# CLI Usage
+# Mount Volume
+# sudo veracrypt -t ~/Documents/<veracrypt_volume> /mnt/<directory>
+#
+# List Mounted Volumes
+# veracrypt -t -l
+#
+# Unmount Volume
+# veracrypt -t -d /mnt/<directory>
+
+
+# dependencies
+sudo apt install yasm libgtk-3-dev make g++ gcc libfuse-dev --no-install-recommends
+
+DIR="/home/pborges/build/VeraCrypt"
+
+[ ! -d $DIR ] && mkdir $DIR
+
+if [ -d "$DIR/VeraCrypt" ]; then
+    cd "$DIR/VeraCrypt"
+    printf "> Updating Git Repository: VeraCrypt\n"
+    git pull
+else
+    cd $DIR
+    git clone https://github.com/veracrypt/VeraCrypt.git
+fi
+
+if [ -d "$DIR/wxWidgets" ]; then
+    cd "$DIR/wxWidgets"
+    printf "> Updating Git Repository: wxWidgets\n"
+    git pull
+else
+    cd $DIR
+    git clone https://github.com/wxWidgets/wxWidgets.git --recurse-submodules
+fi
+
+printf "> Building Veracrypt...\n"
+cd "$DIR/VeraCrypt/src"
+make NOGUI=1 WXSTATIC=1 WX_ROOT=$DIR/wxWidgets/ wxbuild
+make NOGUI=1 WXSTATIC=1
+printf "> Build complete!\n"
+printf "> The 'veracrypt' binary should be under '$DIR/VeraCrypt/src/Main/'\n"
