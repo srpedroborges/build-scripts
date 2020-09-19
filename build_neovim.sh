369d39236294420927478bab24e94d8408c15b00
srpedroborges <55651417+srpedroborges@users.noreply.github.com>
2020-09-19 12:57:31 +0100

    Add files via upload

---
+++ build_neovim.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+###############################################################
+#
+# Build NeoVim on Debian 10.5 / Ubuntu 20.04
+#
+###############################################################
+
+# Reference
+
+# dependencies
+# xclip -> for clipboard functionality 
+sudo apt install make gcc libtool libtool-bin gettext xclip --no-install-recommends
+
+DIR="/home/pborges/build/"
+WORKFOLDER="neovim"
+
+[ ! -d $DIR ] && mkdir -p $DIR
+
+if [ -d "$DIR/$WORKFOLDER" ]; then
+    printf "> Updating existing repository: $WORKFOLDER\n"
+    cd $DIR/$WORKFOLDER
+    git pull
+else
+    cd $DIR
+    git clone https://github.com/neovim/neovim.git
+fi
+printf "> Building and installing\n"
+sudo make uninstall
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+printf "> Run ':checkhealth' inside nvim to check status\n"
+printf "> Complete!"
