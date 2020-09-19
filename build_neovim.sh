c306019cfae07c410d23248a7b47e4d6071cf420
srpedroborges <55651417+srpedroborges@users.noreply.github.com>
2020-09-19 13:59:04 +0100

    Add files via upload

--- build_neovim.sh
+++ build_neovim.sh
@@ -12,7 +12,8 @@
 # xclip -> for clipboard functionality 
 sudo apt install m4 automake unzip make gcc g++ pkg-config cmake libtool libtool-bin gettext xclip --no-install-recommends
 
-DIR="/home/pborges/build/"
+USER="pborges"
+DIR="/home/$USER/build/"
 WORKFOLDER="neovim"
 
 [ ! -d $DIR ] && mkdir -p $DIR
@@ -29,5 +30,9 @@ printf "> Building and installing\n"
 cd $DIR/$WORKFOLDER
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
+printf "> Setting up default options\n"
+mkdir -p /home/$USER/.config/nvim/
+echo "set number" > /home/$USER/.config/nvim/vimrc
+echo "set relativenumber" >> /home/$USER/.config/nvim/vimrc
 printf "> Run ':checkhealth' inside nvim to check status\n"
 printf "> Complete!\n"
