51f4b12b90f6d1b9b8f0e1c8c81f1b0cb2186e8a
srpedroborges <55651417+srpedroborges@users.noreply.github.com>
2020-10-11 08:04:39 +0100

    Add files via upload

--- build_streamlink.sh
+++ build_streamlink.sh
@@ -2,6 +2,12 @@
 
 # https://streamlink.github.io/install.html#source-code
 
+# To uninstall, remove streamlink directory in:
+# /usr/local/lib/python3.8/dist-packages
+# and the binary in:
+# /usr/local/bin/streamlink
+
+
 # System wide configuration
 
 DIR="/home/pborges/build/streamlink"
