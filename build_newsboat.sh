af6d60ee9c3e841c3b88c41284e195c28a8fa823
srpedroborges <55651417+srpedroborges@users.noreply.github.com>
2020-12-05 18:58:21 +0000

    Add files via upload

--- build_newsboat.sh
+++ build_newsboat.sh
@@ -6,7 +6,7 @@
 
 
 # Dependencies
-sudo apt install libcurl4-openssl-dev libxml2-dev libstfl-dev libjson-c-dev libssl-dev libncurses5-dev libsqlite3-dev asciidoctor --no-install-recommends
+sudo apt install git libcurl4-openssl-dev libxml2-dev libstfl-dev libjson-c-dev libssl-dev libncurses5-dev libsqlite3-dev asciidoctor --no-install-recommends
 # It also requires rust - https://www.rust-lang.org/tools/install
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
@@ -20,5 +20,10 @@ else
 fi
 
 cd $DIR
+# Patch Makefile to overcome the following error:
+# src/feedlistformaction.cpp:277:34: error: ‘*((void*)& exit_code +1)’ may be used uninitialized in this function [-Werror=maybe-uninitialized]
+# cc1plus: all warnings being treated as errors
+sed -i "s/Werror/Wno-error/g" Makefile
+
 make -j4
 sudo make install
