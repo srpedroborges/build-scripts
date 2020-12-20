14b1a5eece8f04ae3b1e4bce7c0741d2e7040898
srpedroborges <55651417+srpedroborges@users.noreply.github.com>
2020-12-20 09:36:08 +0000

    Add files via upload

--- build_newsboat.sh
+++ build_newsboat.sh
@@ -12,18 +12,23 @@ sudo apt install git libcurl4-openssl-dev libxml2-dev libstfl-dev libjson-c-dev 
 
 
 DIR="/home/pborges/build/newsboat"
+
+
+# Had to change the logic, since we have to modify the `Makefile` to void error on warning.
 if [ -d "$DIR" ]; then
-     cd "$DIR"
-     git pull
-else
-    git clone https://github.com/newsboat/newsboat.git
+     #cd "$DIR"
+     #git pull
+    rm -rf $DIR
 fi
 
+git clone https://github.com/newsboat/newsboat.git
+
 cd $DIR
-# Patch Makefile to overcome the following error:
+# Patrch Makefile to overcome the following error:
 # src/feedlistformaction.cpp:277:34: error: ‘*((void*)& exit_code +1)’ may be used uninitialized in this function [-Werror=maybe-uninitialized]
 # cc1plus: all warnings being treated as errors
 sed -i "s/Werror/Wno-error/g" Makefile
 
 make -j4
 sudo make install
+
